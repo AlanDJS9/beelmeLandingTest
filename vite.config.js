7df135000e5e40ee5a235c39cc16f673610f09fc
Alan David Jiménez Sánchez <47157146+AlanDJS9@users.noreply.github.com>
2024-12-21 16:46:01 -0600

    vite configured

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://AlanDJS9.github.io/beelmeLandingTest', // Link page
 })
